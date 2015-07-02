f500b97600e5d2b16d255a8fc8b0de4f94510f66
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-02 10:52:53 -0400

    Add iter to Generator

--- src/lib.rs
+++ src/lib.rs
@@ -6,28 +6,50 @@
 /// use random::Generator;
 ///
 /// let mut generator = random::default().seed([42, 69]);
-/// let uniforms = (0..100).map(|_| generator.next::<f64>()).collect::<Vec<_>>();
+/// let uniforms = generator.iter().take(100).collect::<Vec<f64>>();
 /// ```
 
 use std::cell::RefCell;
+use std::marker::PhantomData;
 use std::rc::Rc;
 
 /// A source of randomness.
-pub trait Generator {
-    /// Read the next chunk.
+pub trait Generator: Sized {
+    /// Return the next raw chunk.
     fn read(&mut self) -> u64;
 
-    /// Read the next quantity.
+    /// Return the next quantity.
     #[inline(always)]
-    fn next<T: Quantity>(&mut self) -> T {
+    fn take<T: Quantity>(&mut self) -> T {
         Quantity::make(self.read())
     }
+
+    /// Return a sequence of quantities.
+    #[inline]
+    fn iter<'l, T: Quantity>(&'l mut self) -> Sequence<'l, Self, T> {
+        Sequence { generator: self, phantom: PhantomData }
+    }
+}
+
+/// A sequence of random quantities.
+pub struct Sequence<'l, G, Q> where G: Generator + 'l, Q: Quantity + 'l {
+    generator: &'l mut G,
+    phantom: PhantomData<&'l Q>,
 }
 
 /// A random quantity.
 pub trait Quantity {
-    /// Make up a random quantity.
+    /// Make up a quantity from a raw chunk.
     fn make(u64) -> Self;
+}
+
+impl<'l, G, Q> Iterator for Sequence<'l, G, Q> where G: Generator, Q: Quantity {
+    type Item = Q;
+
+    #[inline(always)]
+    fn next(&mut self) -> Option<Q> {
+        Some(self.generator.take())
+    }
 }
 
 impl Quantity for f64 {
@@ -44,11 +66,11 @@ impl Quantity for u64 {
     }
 }
 
-/// The default generator, which is the Xorshift+ algorithm.
+/// The default source, which is the Xorshift+ algorithm.
 pub struct Default(Rc<RefCell<XorshiftPlus>>);
 
 impl Default {
-    /// Seed the generator.
+    /// Seed the source.
     #[inline(always)]
     pub fn seed(self, seed: [u64; 2]) -> Default {
         *self.0.borrow_mut() = XorshiftPlus::new(seed);
@@ -61,18 +83,13 @@ impl Generator for Default {
     fn read(&mut self) -> u64 {
         self.0.borrow_mut().read()
     }
-
-    #[inline(always)]
-    fn next<T: Quantity>(&mut self) -> T {
-        self.0.borrow_mut().next()
-    }
 }
 
-/// Return the default generator.
+/// Return the default source.
 ///
-/// Each thread has its own copy of the generator, and each copy is initialized
+/// Each thread has its own copy of the source, and each copy is initialized
 /// with the same default seed. Consequently, the usage is thread safe; however,
-/// each thread is responsible for reseeding its default generator.
+/// each thread is responsible for reseeding its source.
 #[inline(always)]
 pub fn default() -> Default {
     thread_local!(static DEFAULT: Rc<RefCell<XorshiftPlus>> = {
@@ -82,17 +99,4 @@ pub fn default() -> Default {
 }
 
 mod xorshift;
-
 pub use self::xorshift::XorshiftPlus;
-
-#[cfg(test)]
-mod tests {
-    use {Generator, XorshiftPlus};
-
-    #[test]
-    fn next() {
-        let mut generator = XorshiftPlus::new([42, 69]);
-        let _ = generator.next::<f64>();
-        let _ = generator.next::<u64>();
-    }
-}
