8c263115266ef968ff5c8fdc26c518f367439e82
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-03 14:34:47 -0400

    Drop Self: Sized

--- src/lib.rs
+++ src/lib.rs
@@ -20,13 +20,13 @@ pub trait Source {
 
     /// Return the next quantity.
     #[inline(always)]
-    fn take<T: Quantity>(&mut self) -> T where Self: Sized {
+    fn take<T: Quantity>(&mut self) -> T {
         Quantity::make(self.read())
     }
 
     /// Return a sequence of quantities.
     #[inline]
-    fn iter<'l, T: Quantity>(&'l mut self) -> Sequence<'l, Self, T> where Self: Sized {
+    fn iter<'l, T: Quantity>(&'l mut self) -> Sequence<'l, Self, T> {
         Sequence { source: self, phantom: PhantomData }
     }
 }
